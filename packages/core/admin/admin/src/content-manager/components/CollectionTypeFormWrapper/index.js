9e3ff7f682d936836912413b5cd50d66837ddb96
Simone Taeggi <startae14@gmail.com>
2022-12-23 10:54:32 +0100

    replace axiosInstance in the content manager collection type form wrapper to publish an entry with the getFetchClient post method

--- packages/core/admin/admin/src/content-manager/components/CollectionTypeFormWrapper/index.js
+++ packages/core/admin/admin/src/content-manager/components/CollectionTypeFormWrapper/index.js
@@ -321,13 +321,14 @@ const CollectionTypeFormWrapper = ({ allLayoutData, children, slug, id, origin }
   }, [displayErrors, id, slug, dispatch]);
 
   const onPublish = useCallback(async () => {
+    const { post } = getFetchClient();
     try {
       trackUsageRef.current('willPublishEntry');
       const endPoint = getRequestUrl(`collection-types/${slug}/${id}/actions/publish`);
 
       dispatch(setStatus('publish-pending'));
 
-      const { data } = await axiosInstance.post(endPoint);
+      const { data } = await post(endPoint);
 
       trackUsageRef.current('didPublishEntry');
 
