9328f8f731b88be160cb04d44cc74025230ec1f8
Simone Taeggi <startae14@gmail.com>
2022-12-23 17:21:47 +0100

    replace axiosInstance in the content manager SingleTypeFormWrapper to publish a single type entry with the getFetchClient post method

--- packages/core/admin/admin/src/content-manager/components/SingleTypeFormWrapper/index.js
+++ packages/core/admin/admin/src/content-manager/components/SingleTypeFormWrapper/index.js
@@ -262,13 +262,14 @@ const SingleTypeFormWrapper = ({ allLayoutData, children, slug }) => {
   }, [displayErrors, slug, dispatch]);
 
   const onPublish = useCallback(async () => {
+    const { post } = getFetchClient();
     try {
       trackUsageRef.current('willPublishEntry');
       const endPoint = getRequestUrl(`${slug}/actions/publish${searchToSend}`);
 
       dispatch(setStatus('publish-pending'));
 
-      const { data } = await axiosInstance.post(endPoint);
+      const { data } = await post(endPoint);
 
       trackUsageRef.current('didPublishEntry');
       toggleNotification({
